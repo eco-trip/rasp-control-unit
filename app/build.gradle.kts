65baf01aa850b805b1dc9f71d2908cb82a45c389
Alberto Marfoglia <alberto.marfoglia@studio.unibo.it>
2022-07-12 14:48:07 +0000

    build: add github packages repo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,18 +24,25 @@ tasks.named("build") {
 
 // Dependencies for this application, until V2 is released, snapshot is required here
 repositories {
+    maven {
+        url = uri("https://maven.pkg.github.com/meboo/pi4j-v2-issue26")
+        credentials {
+            username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+            password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+        }
+    }
     mavenCentral()
 }
 
 dependencies {
-    // implementation 'org.slf4j:slf4j-api:1.7.32'
-    // implementation 'org.slf4j:slf4j-simple:1.7.32'
-    // implementation 'com.pi4j:pi4j-core:2.1.1'
-    // implementation 'com.pi4j:pi4j-plugin-raspberrypi:2.1.1'
-    // implementation 'com.pi4j:pi4j-plugin-pigpio:2.1.1'
+    implementation("org.slf4j:slf4j-api:1.7.32")
+    implementation("org.slf4j:slf4j-simple:1.7.32")
+    implementation("io.github.meboo.pi4j:pi4j-core:2.2.0-issue26")
+    implementation("io.github.meboo.pi4j:pi4j-plugin-raspberrypi:2.2.0-issue26")
+    implementation("io.github.meboo.pi4j:pi4j-plugin-pigpio:2.2.0-issue26")
     // api fileTree(dir: 'lib', includes: ['*.jar'])
     implementation("com.jcraft:jsch:0.1.44-1")
-    implementation(fileTree("libs"))
+    // implementation(fileTree("libs"))
     /*
     implementation files('libs/pi4j-core.jar')
     implementation files('libs/pi4j-core-javadocs.jar')
