d1970a370191ae71c5cf4f7cd1586b8f7857f2b9
Alberto Marfoglia <alberto.marfoglia@studio.unibo.it>
2022-08-26 18:07:16 +0200

    feat(domain): add Current measure

---
+++ domain/src/main/java/io/github/ecotrip/measure/energy/Current.java
@@ -0,0 +1,35 @@
+package io.github.ecotrip.measure.energy;
+
+import io.github.ecotrip.measure.CombinableMeasure;
+import io.github.ecotrip.measure.MeasureType;
+
+/**
+ * Current measure in amps
+ */
+public class Current extends CombinableMeasure {
+
+    private Current(final double value) {
+        super(value, MeasureType.CURRENT);
+    }
+
+    /**
+     * Factory method
+     * @param amps current in amps
+     * @return a new Current value object
+     */
+    public static Current of(double amps) {
+        return new Current(amps);
+    }
+
+    @Override
+    public String toString() {
+        return "Current{"
+                + "value=" + getValue() + " amps"
+                + '}';
+    }
+
+    @Override
+    protected CombinableMeasure combine(CombinableMeasure with) {
+        return Current.of(getValue() + with.getValue());
+    }
+}
