8c13c43f8e20c48ff7bfe6e6150b6c6de6aa2dce
MEBoo <alan@meblabs.com>
2022-10-31 19:22:00 +0100

    fix(authorization): stack overflow on auth service

--- authorization/src/main/java/io/github/ecotrip/AuthorizationService.java
+++ authorization/src/main/java/io/github/ecotrip/AuthorizationService.java
@@ -1,6 +1,10 @@
 package io.github.ecotrip;
 
+import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import io.github.ecotrip.execution.engine.Engine;
 import io.github.ecotrip.pattern.Observer;
@@ -11,6 +15,7 @@ import io.github.ecotrip.usecase.AuthorizationUseCases;
  * Contains the logic to receive and transmit a token to an NFC device
  */
 public class AuthorizationService implements Observer<Token> {
+    private final Logger logger = Logger.getLogger(AuthorizationService.class.getName());
     private final AuthorizationUseCases authorizationUseCases;
     private final Engine engine;
 
@@ -29,9 +34,16 @@ public class AuthorizationService implements Observer<Token> {
     }
 
     private CompletableFuture<Void> startNfcTagEmulation() {
-        return authorizationUseCases.waitNearbyDevice()
-                .thenCompose(u -> authorizationUseCases.transmitToken())
-                .thenCompose(u -> this.startNfcTagEmulation());
+        return CompletableFuture.runAsync(() -> {
+            while (true) {
+                try {
+                    authorizationUseCases.waitNearbyDevice().join();
+                    authorizationUseCases.transmitToken().join();
+                } catch (CancellationException | CompletionException ex) {
+                    logger.log(Level.WARNING, ex.getMessage());
+                }
+            }
+        });
     }
 
     public static AuthorizationService of(final Engine engine, final AuthorizationUseCases useCases) {
