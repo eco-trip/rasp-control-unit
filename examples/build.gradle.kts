977176a2ada03c2d735e97efed2a6f1b385d4aea
MEBoo <alan@meblabs.com>
2022-12-06 12:40:29 +0100

    feat(examples): aws adapter

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -5,11 +5,40 @@ plugins {
 group = "io.github.ecotrip"
 
 repositories {
+    maven {
+        url = uri("https://maven.pkg.github.com/meboo/pi4j-v2-issue26")
+        credentials {
+            username = project.findProperty("gpr.user") as String? ?: System.getenv("GH_PACKAGES_USERNAME")
+            password = project.findProperty("gpr.key") as String? ?: System.getenv("GH_PACKAGES_TOKEN")
+        }
+    }
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(libs.junit.jupiter)
+    implementation(project(":utils"))
+    implementation(project(":domain"))
+    implementation(project(":core"))
+    implementation(project(":room-monitoring"))
+    implementation(project(":authorization"))
+    implementation("org.slf4j:slf4j-api:2.0.0-alpha1")
+    implementation("org.slf4j:slf4j-simple:2.0.0-alpha1")
+    implementation("io.github.meboo.pi4j:pi4j-core:2.2.0-issue26")
+    implementation("io.github.meboo.pi4j:pi4j-plugin-raspberrypi:2.2.0-issue26")
+    implementation("io.github.meboo.pi4j:pi4j-plugin-pigpio:2.2.0-issue26")
+    implementation("io.github.meboo.pi4j:pi4j-plugin-linuxfs:2.2.0-issue26")
+    implementation("com.jcraft:jsch:0.1.44-1")
+    testImplementation("io.github.meboo.pi4j:pi4j-test:2.2.0-issue26")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.7")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.7")
+    implementation("commons-codec:commons-codec:1.15")
+    implementation("software.amazon.awssdk.iotdevicesdk:aws-iot-device-sdk:1.10.1")
+}
+
+spotbugs {
+    excludeFilter.set(file("config/spotbugs-exclude.xml"));
 }
 
 tasks.withType<Test> {
---
+++ examples/src/main/java/io/github/ecotrip/aws/AwsAdapter.java
@@ -0,0 +1,119 @@
+package io.github.ecotrip.aws;
+
+import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.github.ecotrip.Generated;
+import io.github.ecotrip.adapter.InputAdapter;
+import io.github.ecotrip.adapter.OutputAdapter;
+import io.github.ecotrip.token.Token;
+import software.amazon.awssdk.crt.mqtt.MqttClientConnection;
+import software.amazon.awssdk.crt.mqtt.MqttMessage;
+import software.amazon.awssdk.crt.mqtt.QualityOfService;
+import software.amazon.awssdk.iot.iotshadow.IotShadowClient;
+import software.amazon.awssdk.iot.iotshadow.model.*;
+
+/**
+ * Amazon Web Services input and output adapter
+ */
+@Generated
+public class AwsAdapter extends InputAdapter implements OutputAdapter<String, String> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AwsAdapter.class);
+    private static final String TOKEN_PROPERTY = "token";
+    private final IotShadowClient shadow;
+    private final MqttClientConnection connection;
+    private final String thingName;
+
+    private AwsAdapter(final MqttClientConnection connection, final String thingName) {
+        this.shadow = new IotShadowClient(connection);
+        this.connection = connection;
+        this.thingName = thingName;
+    }
+
+    @Override
+    public CompletableFuture<Void> sendMessage(String topic, String message) {
+        var msg = new MqttMessage(topic, message.getBytes(), QualityOfService.AT_LEAST_ONCE, false);
+        return connection.publish(msg).thenRun(() -> {});
+    }
+
+    /**
+     * connect to AWS service
+     * @return connection future
+     */
+    public CompletableFuture<Void> connect() {
+        return connection.connect()
+                .thenRun(() -> LOGGER.info("AWS IoT adapter connected"))
+                .thenRun(this::subscribeToGetShadow)
+                .thenRun(this::subscribeToUpdateShadow);
+    }
+
+    public CompletableFuture<Void> disconnect() {
+        return connection.disconnect().thenRun(() -> LOGGER.info("AWS IoT adapter disconnected"));
+    }
+
+    @Override
+    public CompletableFuture<Void> requireToken() {
+        var getShadowRequest = new GetShadowRequest();
+        getShadowRequest.thingName = thingName;
+        return shadow.PublishGetShadow(getShadowRequest, QualityOfService.AT_LEAST_ONCE)
+                .thenRun(() -> LOGGER.info("Send GET/shadow request"));
+    }
+
+    private void subscribeToGetShadow() {
+        var requestGetShadow = new GetShadowSubscriptionRequest();
+        requestGetShadow.thingName = thingName;
+        shadow.SubscribeToGetShadowAccepted(
+                requestGetShadow,
+                QualityOfService.AT_LEAST_ONCE,
+                this::onGetShadowAccepted
+        ).thenRun(() -> LOGGER.info("Subscribed to GET/shadow"));
+    }
+
+    private void subscribeToUpdateShadow() {
+        var requestUpdateShadow = new UpdateShadowSubscriptionRequest();
+        requestUpdateShadow.thingName = thingName;
+        shadow.SubscribeToUpdateShadowAccepted(
+                requestUpdateShadow,
+                QualityOfService.AT_LEAST_ONCE,
+                this::onUpdateShadowAccepted
+        ).thenRun(() -> LOGGER.info("Subscribed to PUT/shadow"));
+    }
+
+    private void onUpdateShadowAccepted(UpdateShadowResponse response) {
+        if (response.state != null) {
+            handleStateResponse(response.state.reported);
+        }
+    }
+
+    private void onGetShadowAccepted(final GetShadowResponse response) {
+        if (response.state != null) {
+            handleStateResponse(response.state.reported);
+        }
+    }
+
+    private void handleStateResponse(final HashMap<String, Object> state) {
+        if (state != null && state.containsKey(TOKEN_PROPERTY)) {
+            var shadowToken = state.get(TOKEN_PROPERTY).toString();
+            notifyObservers(Token.of(shadowToken));
+            LOGGER.info("Received shadow token: " + shadowToken);
+        } else {
+            LOGGER.warn("Received invalid or empty token");
+        }
+    }
+
+    /**
+     * AWSAdapter builder
+     * @param connection MqttClientConnection
+     * @param thingName thing name
+     * @return the AWSAdapter
+     */
+    public static AwsAdapter of(final MqttClientConnection connection, final String thingName) {
+        if (connection == null) {
+            throw new RuntimeException("MQTT connection creation failed!");
+        }
+        return new AwsAdapter(connection, thingName);
+    }
+}
